6e0bee45e792f5051f3b467801b2c9d835a25efc
dimkungz <dimkungz@gmail.com>
2026-07-09 13:30:05 +0700

    feat: integrate UserDataContext for user information in AppHeader and ViewProductPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import React from "react";
+export const UserDataContext = React.createContext()
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
@@ -13,12 +15,17 @@ function App() {
 
   return (
     <div className="App">
+      <UserDataContext.Provider
+        value={{
+          user: userData
+        }}>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/view/:id" element={<ViewProductPage />} />
         </Routes>
       </Router>
+      </UserDataContext.Provider>
     </div>
   );
 }
--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
+import { UserDataContext } from "../App.jsx";
+import { useContext } from "react";
 
 function ViewProductPage() {
+  const context = useContext(UserDataContext)
   const navigate = useNavigate();
   return (
     <div>
@@ -11,7 +14,7 @@ function ViewProductPage() {
       </div>
 
       <div className="product-promotion-box">
-        <h2>คุณคือสมาชิกในระดับ (x) ดังนั้นคุณได้สิทธิพิเศษลด 50%</h2>
+        <h2>คุณคือสมาชิกในระดับ {context.user.level} ดังนั้นคุณได้สิทธิพิเศษลด 50%</h2>
         <button onClick={() => alert("🥳🥳🥳🥳")}>กดรับสิทธิ</button>
       </div>
 
